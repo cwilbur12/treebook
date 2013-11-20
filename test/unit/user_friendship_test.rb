3c37ba4dd21ff5300b441058723baad279e20c85
cwilbur12 <cwilbur12@gmail.com>
2013-11-20 17:17:18 -0500

    added the friend relationship in the model so it knows its a user and added the tests the userfriendships should be long to a user

--- test/unit/user_friendship_test.rb
+++ test/unit/user_friendship_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserFriendshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should belong_to(:user)
+  should belong_to(:friend)
+
+  test "creating friendship works without raising exception" do 
+  	assert_nothing_raised do
+  	 	UserFriendship.create user: users(:cory), friend: users(:trevor)
+    end
+  end
+
 end
